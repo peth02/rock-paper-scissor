8538c1b0e5e9935a6dc21088059289fbcc19b981
peth <oopeth139oo@gmail.com>
2023-04-06 22:30:14 +0700

    start files

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("hi");
+
+const title = document.getElementById('container');
+console.log(title);
+
+const listitem = document.getElementsByClassName("list-item");
+console.log(listitem);
