55bfadf499bd45065aa96fd2bdf57a7d8ec90f77
peth <oopeth139oo@gmail.com>
2023-04-26 17:58:20 +0700

    add ui button and endless game round

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function getComputerChoice(){
 function playRound(player, computer) {
     player = player.toLowerCase();
     const choice = ["rock", "paper", "scissor"];
-    const winP = ["scissor", "rock", "paper"];
+    const winChoice = ["scissor", "rock", "paper"];
     let i = 0;
-    for(i = 0; i < 3; i++){
+    for(i; i < 3; i++){
         if(player === choice[i]){
             break;
         }
     };
-    if(winP[i] === computer){
+    if(winChoice[i] === computer){
         return "win";
     }else if(player === computer){
         return "draw";
@@ -23,22 +23,90 @@ function playRound(player, computer) {
     }
 }
 
-function game(){
-    let scoreP = 0;
-    let scoreC = 0;
-    while(scoreP != 5 && scoreC != 5){
+// function game(){
+//     let scorePlayer = 0;
+//     let scoreCom = 0;
+//     while(scorePlayer !== 5 && scoreCom !== 5){
 
-        let playerSelection = prompt("choose your weapon");
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
-        if(result === "win"){
-            scoreP += 1;
-        }else if(result === "lose"){
-            scoreC += 1;
-        }
-    };
-    return "player = "+scoreP + " CPU = "+scoreC;
+//         let playerSelection = prompt("choose your weapon");
+//         let computerSelection = getComputerChoice();
+//         let result = playRound(playerSelection, computerSelection);
+//         console.log(result);
+//         if(result === "win"){
+//             scorePlayer += 1;
+//         }else if(result === "lose"){
+//             scoreCom += 1;
+//         }
+//     };
+//     return "player = "+scorePlayer + " CPU = "+scoreCom;
+// }
+
+let scorePlayer = 0;
+let scoreCom = 0;
+let round = 0;
+
+const choices = document.querySelectorAll(".choice");
+const player = document.querySelector(".player");
+const com = document.querySelector(".com");
+const score = document.querySelector(".score");
+
+function game(choice){
+
+    round += 1;
+    let playerSelection = choice.innerText;
+    player.innerText = `player choose ${playerSelection.toLowerCase()}`;
+
+    let computerSelection = getComputerChoice();
+    com.innerText = `com choose ${computerSelection.toLowerCase()}`;
+
+    let result = playRound(playerSelection, computerSelection);
+    // console.log(result);
+
+    if(result === "win"){
+        scorePlayer += 1;
+    }else if(result === "lose"){
+        scoreCom += 1;
+    }
+
+    if(round !== 0){
+        score.innerText = `player = ${scorePlayer} com = ${scoreCom}`
+    }
+    // console.log(`player choose ${scorePlayer} com choose ${scoreCom}`);
+    // if score = 5 end game and restart button
+    if(scorePlayer >= 5 || scoreCom >= 5){
+        // alert("end");
+        choice.removeEventListener("click");
+    }
+
 }
 
-console.log(game());
+choices.forEach(choice => {
+    choice.addEventListener("click", () => {
+        round += 1;
+        let playerSelection = choice.innerText;
+        player.innerText = `player choose ${playerSelection.toLowerCase()}`;
+
+        let computerSelection = getComputerChoice();
+        com.innerText = `com choose ${computerSelection.toLowerCase()}`;
+
+        let result = playRound(playerSelection, computerSelection);
+        // console.log(result);
+
+        if(result === "win"){
+            scorePlayer += 1;
+        }else if(result === "lose"){
+            scoreCom += 1;
+        }
+
+        if(round !== 0){
+            score.innerText = `player = ${scorePlayer} com = ${scoreCom}`
+        }
+        // console.log(`player choose ${scorePlayer} com choose ${scoreCom}`);
+        // if score = 5 end game and restart button
+        if(scorePlayer >= 5 || scoreCom >= 5){
+            // alert("end");
+            choice.removeEventListener("click");
+        }
+    });
+});
+
